cb00d7955a65dfae4511882c9d6226dc4ae72162
mihaela <cholitto_cholitto@yahoo.com>
2021-01-02 20:47:26 +0000

    My changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
+        System.out.println(answer);
 
-        var item = "Shirt";
-        var size = "M";
-        var price = 14.99;
-        var color = "Red";
 
     }
 }
